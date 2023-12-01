c90026f63f7dceb477b9dc7337c0dcef54fbb006
pibragimov <pibragimov@whoosh.bike>
2023-12-01 18:56:55 +0300

    auto: autocommit

--- backend/app/persistence/database.py
+++ backend/app/persistence/database.py
@@ -1,6 +1,5 @@
-from geoalchemy2.types import Geometry
-from sqlalchemy import BigInteger, Column, MetaData, Text
-from sqlalchemy.dialects.postgresql import JSONB, UUID
+from sqlalchemy import BigInteger, Column, MetaData
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
 
 from shared.ulid import ulid_as_uuid
@@ -11,40 +10,10 @@ Base = declarative_base()
 Base.metadata = metadata_obj
 
 
-class GeoObject(Base):
+class Entity(Base):
     __abstract__ = True
 
-    id = Column(  # noqa: A003
-        BigInteger,
-        primary_key=True,
-        comment="id for internal usage, i.e. joins, selects",
-    )  # noqa: A003
+    id_ = Column("id", BigInteger, primary_key=True)
     internal_id = Column(
-        UUID(as_uuid=True),
-        unique=True,
-        default=ulid_as_uuid,
-        nullable=False,
-        comment="id for external usage, i.e. in API",
+        UUID(as_uuid=True), unique=True, default=ulid_as_uuid, nullable=False
     )
-
-    address = Column(Text)
-    coordinate = Column(Geometry(geometry_type="POINT", srid=4326, spatial_index=True))
-    prediction = Column(BigInteger)
-
-
-class ATM(GeoObject):
-    __tablename__ = "atm"
-
-    features = Column(JSONB)
-
-
-class Office(GeoObject):
-    __tablename__ = "office"
-
-    sale_point_name = Column(Text)
-    individual_schedule = Column(JSONB, nullable=False)
-    legal_entity_schedule = Column(JSONB, nullable=False)
-    metro_station = Column(Text)
-    features = Column(JSONB)
-    sale_point_format = Column(Text)
-    office_type = Column(Text)
--- backend/app/presentation/web/schemas.py
+++ backend/app/presentation/web/schemas.py
@@ -1,14 +1,6 @@
 import enum
-import uuid
 
-from pydantic import Field
-
-from schemas.atm import ATM
 from schemas.base import CamelizedBaseModel
-from schemas.geo import Coordinate
-from schemas.office import Office
-from service.view_service import TellerType
-from supplier.ort_supplier import Profiles
 
 
 class HealthStatuses(str, enum.Enum):
@@ -19,24 +11,3 @@ class HealthStatuses(str, enum.Enum):
 class HealthResponse(CamelizedBaseModel):
     status: HealthStatuses
     error: str | None = None
-
-
-class Route(CamelizedBaseModel):
-    start: Coordinate = Field(..., example=Coordinate(lat=55.729414, lng=37.610190))
-    end: Coordinate = Field(..., example=Coordinate(lat=55.733137, lng=37.619521))
-    profile: Profiles = Profiles.FOOT_WALKING
-
-
-class RouteByTeller(CamelizedBaseModel):
-    start: Coordinate = Field(..., example=Coordinate(lat=55.801432, lng=37.702547))
-    profile: Profiles = Profiles.FOOT_WALKING
-
-    teller_id: uuid.UUID = Field(
-        ..., example=uuid.UUID("018b2aee-bb0a-1491-d31d-e3c2802f53d5")
-    )
-    teller_type: TellerType = Field(..., example=TellerType.ATM)
-
-
-class TopTellersResponse(CamelizedBaseModel):
-    atms: list[ATM]
-    offices: list[Office]
